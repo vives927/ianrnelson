df237023a5b45c7c0ecde03602f583c6d3a59e1e
Genevieve Nelson <vives927@gmail.com>
2013-04-20 11:44:01 -0400

    Tooltip should only appear for tooltip items

--- js/global.js
+++ js/global.js
@@ -83,7 +83,7 @@ function bulletNav($el) {
 $(document).ready(function() {
 	accordion($('div.faqPage'));
 	bulletNav($('div.video'));
-	$(document).tooltip();
+	$('a.link_tooltip').tooltip();
 	$('a.link_tooltip').click(function(e) {
 		e.preventDefault();
 	});
