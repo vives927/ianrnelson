056378eef201d6b0c75a45650976558df7a23195
Genevieve Nelson <genevieven@c2mpg.com>
2015-08-11 13:20:48 -0400

    Changing licensure statement; making FAQ page more standard; general clean-up

--- js/faq.js
+++ js/faq.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-    $('div.faqInner').perfectScrollbar();
+(function ($) {
+    'use strict';
+
     $('a.link_tooltip').tooltip();
     $('a.link_tooltip').click(function(e) {
         e.preventDefault();
     });
-});
+
+}(jQuery));
